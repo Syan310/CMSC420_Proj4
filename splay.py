294811186fce80f2010f26e709e9a2b4c4e24eb8
Syan310 <105128575+Syan310@users.noreply.github.com>
2023-11-09 02:05:23 -0500

    Update splay.py

--- splay.py
+++ splay.py
@@ -1,48 +1,58 @@
 from __future__ import annotations
 import json
-from typing import List
-
-verbose = False
+from typing import Optional
 
 class Node:
-    def __init__(self, key: int, leftchild=None, rightchild=None, parent=None):
+    def __init__(self, key: int, leftchild: Optional[Node] = None, rightchild: Optional[Node] = None, parent: Optional[Node] = None):
         self.key = key
         self.leftchild = leftchild
         self.rightchild = rightchild
         self.parent = parent
 
 class SplayTree:
-    def __init__(self, root: Node = None):
+    def __init__(self, root: Optional[Node] = None):
         self.root = root
 
     def dump(self) -> str:
-        def _to_dict(node) -> dict:
-            pk = None
-            if node.parent:
-                pk = node.parent.key
-            return {
-                "key": node.key,
-                "left": (_to_dict(node.leftchild) if node.leftchild else None),
-                "right": (_to_dict(node.rightchild) if node.rightchild else None),
-                "parentkey": pk
-            }
-        return json.dumps(_to_dict(self.root) if self.root else {}, indent=2)
-
-    def _splay(self, node):
-        while node.parent:  # Continue until node is the root
-            if node == node.parent.leftchild:
+            def _to_dict(node: Optional[Node]) -> dict:
+                if not node:
+                    return None
+                pk = node.parent.key if node.parent else None
+                return {
+                    "key": node.key,
+                    "left": _to_dict(node.leftchild),
+                    "right": _to_dict(node.rightchild),
+                    "parentkey": pk
+                }
+            return json.dumps(_to_dict(self.root), indent=2)
+        
+    def _splay(self, node: Optional[Node]):
+        while node.parent:
+            if not node.parent.parent:
+                if node.parent.leftchild == node:
+                    self._right_rotate(node.parent)
+                else:
+                    self._left_rotate(node.parent)
+            elif node.parent.leftchild == node and node.parent.parent.leftchild == node.parent:
+                self._right_rotate(node.parent.parent)
                 self._right_rotate(node.parent)
+            elif node.parent.rightchild == node and node.parent.parent.rightchild == node.parent:
+                self._left_rotate(node.parent.parent)
+                self._left_rotate(node.parent)
+            elif node.parent.leftchild == node and node.parent.parent.rightchild == node.parent:
+                self._right_rotate(node.parent)
+                self._left_rotate(node.parent)
             else:
                 self._left_rotate(node.parent)
+                self._right_rotate(node.parent)
 
-   
-    def _left_rotate(self, x):
+    def _left_rotate(self, x: Node):
         y = x.rightchild
         x.rightchild = y.leftchild
-        if y.leftchild is not None:
+        if y.leftchild:
             y.leftchild.parent = x
         y.parent = x.parent
-        if x.parent is None:
+        if not x.parent:
             self.root = y
         elif x == x.parent.leftchild:
             x.parent.leftchild = y
@@ -51,13 +61,13 @@ class SplayTree:
         y.leftchild = x
         x.parent = y
 
-    def _right_rotate(self, x):
+    def _right_rotate(self, x: Node):
         y = x.leftchild
         x.leftchild = y.rightchild
-        if y.rightchild is not None:
+        if y.rightchild:
             y.rightchild.parent = x
         y.parent = x.parent
-        if x.parent is None:
+        if not x.parent:
             self.root = y
         elif x == x.parent.rightchild:
             x.parent.rightchild = y
@@ -66,11 +76,10 @@ class SplayTree:
         y.rightchild = x
         x.parent = y
 
-    def search(self, key: int) -> Node:
+    def _find_node(self, key: int) -> Optional[Node]:
         node = self.root
         while node:
             if key == node.key:
-                self._splay(node)
                 return node
             elif key < node.key:
                 node = node.leftchild
@@ -78,60 +87,89 @@ class SplayTree:
                 node = node.rightchild
         return None
 
-    def insert(self, key: int):
-        node = self.root
-        parent = None
-        while node is not None:
-            parent = node
-            if key < node.key:
-                node = node.leftchild
-            elif key > node.key:
-                node = node.rightchild
-            else:  # Key already exists, splay the node and return.
-                self._splay(node)
-                return
-
-        new_node = Node(key, parent=parent)
-        if parent is None:
-            self.root = new_node
-        elif key < parent.key:
-            parent.leftchild = new_node
-        else:
-            parent.rightchild = new_node
-
-        self._splay(new_node)
-        
-    def delete(self, key: int):
-        node = self.search(key)
-        if node:
-            self._splay(node)  # Splay the node to be deleted to the root
-            if not node.leftchild:
-                self._transplant(node, node.rightchild)
-            elif not node.rightchild:
-                self._transplant(node, node.leftchild)
-            else:
-                y = self._minimum(node.rightchild)
-                if y.parent != node:
-                    self._transplant(y, y.rightchild)
-                    y.rightchild = node.rightchild
-                    y.rightchild.parent = y
-                self._transplant(node, y)
-                y.leftchild = node.leftchild
-                y.leftchild.parent = y
-            del node
-
-    def _transplant(self, u, v):
-        if not u.parent:
-            self.root = v
-        elif u == u.parent.leftchild:
-            u.parent.leftchild = v
-        else:
-            u.parent.rightchild = v
-        if v:
-            v.parent = u.parent
-
-    def _minimum(self, node):
-        while node.leftchild:
+    def _find_min(self, node: Optional[Node]) -> Optional[Node]:
+        while node and node.leftchild:
             node = node.leftchild
         return node
 
+    def _find_max(self, node: Optional[Node]) -> Optional[Node]:
+        while node and node.rightchild:
+            node = node.rightchild
+        return node
+
+    def insert(self, key: int):
+        new_node = Node(key)
+        if not self.root:
+            self.root = new_node
+            return
+
+        node = self._find_node(key)
+        if node:
+            self._splay(node)
+            return  # Node already in the tree, so we splay and exit.
+
+        # Find in-order predecessor if it exists
+        predecessor = self._find_max(self.root.leftchild)
+        if predecessor:
+            self._splay(predecessor)
+            predecessor.rightchild = new_node
+            new_node.parent = predecessor
+            return
+
+        # Find in-order successor if it exists
+        successor = self._find_min(self.root.rightchild)
+        if successor:
+            self._splay(successor)
+            successor.leftchild = new_node
+            new_node.parent = successor
+            return
+
+        # If there's no predecessor or successor, the tree is a single node
+        # Insert the new node as the appropriate child of the root.
+        if key < self.root.key:
+            self.root.leftchild = new_node
+        else:
+            self.root.rightchild = new_node
+        new_node.parent = self.root
+
+
+    def search(self, key: int):
+        node = self._find_node(key)
+        if node:
+            self._splay(node)
+        return node
+
+    def delete(self, key: int):
+        node = self._find_node(key)
+        if not node:
+            return  # Node not found, nothing to delete.
+
+        self._splay(node)
+        if not node.leftchild and not node.rightchild:
+            # Node is the only node in the tree.
+            self.root = None
+        elif not node.leftchild or not node.rightchild:
+            # Node has only one child.
+            self.root = node.leftchild if node.leftchild else node.rightchild
+            self.root.parent = None
+        else:
+            # Node has two children.
+            successor = self._find_min(node.rightchild)
+            if successor:
+                if successor.parent != node:
+                    self._transplant(successor, successor.rightchild)
+                    successor.rightchild = node.rightchild
+                    successor.rightchild.parent = successor
+                self._transplant(node, successor)
+                successor.leftchild = node.leftchild
+                successor.leftchild.parent = successor
+            else:
+                # If there's no successor, then node is the maximum and has only a left child.
+                self.root = node.leftchild
+                self.root.parent = None
+
+        del node  # Node is now disconnected from the tree and can be deleted.
+
+    
+
+# This code should be tested thoroughly to ensure its correctness.
